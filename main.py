c8a2d8fddc16efc63c8e468ccac0c330a7dd217b
DannyLKa <dchernuh2005@gmail.com>
2025-10-02 19:37:20 +0500

    Комит

---
+++ main.py
@@ -0,0 +1,15 @@
+n = int(input("Введите количество чисел: "))
+
+numbers = []
+for i in range(n):
+    num = int(input(f"Введите {i+1} число: "))
+    numbers.append(num)
+
+print("Изначальный массив: ", numbers)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+
+print("Отсортированный массив: ", numbers)
